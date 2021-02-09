8e8487e9e1ef1835db088bf3198929a1d4b7d670
mtrentma <59029140+mtrentma@users.noreply.github.com>
2021-02-09 11:36:01 -0700

    Update time-series-missing-data-state-space model.R

--- time-series-missing-data-state-space model.R
+++ time-series-missing-data-state-space model.R
@@ -12,9 +12,9 @@ library(lubridate)
 library(data.table)
 
 ###Simulate data
-N<-366 #length of data
+N<-731 #length of data
 t<-0:N #time
-time<-seq(as.POSIXct("2020/1/1"),as.POSIXct("2020/12/31"), by="day") #for light
+time<-seq(as.POSIXct("2020/1/1"),as.POSIXct("2021/12/31"), by="day") #for light
 
 ##light
 # From Yard et al. (1995) Ecological Modelling.  Remember your trig?  
@@ -52,31 +52,38 @@ length(fake.turb$turb)
 turb<-fake.turb$turb
 
 ##GPP based on time-series model with known paramters
-set.seed(430)
+set.seed(360)
 x<-NA
-b0<-15 #intercept
-phi<-.8 #autoregressive parameter
-b1<-.5 #light parameter
-b2<--.05 #turbidity parameter
-sd.p<-0.6 #process error
-x[1]=15 #for initialization
+b0<-1#intercept
+phi<-0.9 #autoregressive parameter
+b1<-0.8 #light parameter
+#b2<--.05 #turbidity parameter
+sd.p<-0.5#process error
+x[1]<-25 #for initialization
 
 #Estimate latent state
 for (t in 2:N){
-        x[t] = phi*x[t-1]+b1*light[t]+rnorm(1, 0, sd.p)
+       x[t] = b0+phi*x[t-1]+b1*light[t]+rnorm(1, 0, sd.p)
 }
+#Estimate latent state
+#for (t in 2:N){
+#        x[t] = b0+phi*x[t-1]+rnorm(1, 0, sd.p)
+#}
+
+
 #Estimate observed data
-sd.o<-0.7
+set.seed(360)
+sd.o<-0.5
 sdo <-rnorm(N, 0, sd.o)
-y <- x + sdo
+y <-x + sdo
 
 sdo_sd<-mean(abs(sdo))
 
 ##Plot observed and latent
-plot(1:N, x[1:366],
+plot(1:N, x[1:N],
      pch=3, cex = 0.8, col="blue", ty="o", lty = 3,
      xlab = "t", ylab = expression(z[t]),
-     xlim = c(0,N), ylim = c(min(y), max(y)+max(y)/5),
+     xlim = c(0,N), ylim = c(min(y), max(y)),
      las = 1)
 points(1:N, y,
        pch = 19, cex = 0.7, col="red", ty = "o")
@@ -87,16 +94,28 @@ legend("top",
        lty = c(3, 1),
        horiz=TRUE, bty="n", cex=0.9)
 
+#Center data
+y<-(y-mean(y))/sd(y)
+mean(y)
+sd(y)
+
+x<-(x-mean(x))/sd(x)
+mean(x)
+sd(x)
+
+
+plot(1:N,y)
+lines(1:N,x, color="blue")
 ##Force some missing data
 #Note-the model will still work if no data is missing but the objects in the next block of code (i.e., "y_index_mis", etc.)
 #still need to be created
 
 set.seed(430)
 #Store simulated data in a list 
-y_miss<-list(y,y,y,y,y,y,y)
+y_miss<-list(y,y,y,y,y,y,y,y)
 
 #vector of missing number amounts
-missing_n<-c(10,25,50,75,100,150,175)
+missing_n<-c(0,10,25,50,75,100,150,175)
 
 #Create a list to store missing data integers
 y_missing_integers<-c()
@@ -107,16 +126,18 @@ y_missing_integers[[1]]<- which(y %in% sample(y,missing_n[1]))
 #Adds the previous missing data integers to the next set. This makes them nested.
 #i.e, all the missing data 10 integers are added to 15 more to make missing data 15.
 for(i in 2:length(missing_n)){
-x<-which(y %in% sample(y,(missing_n[i]-missing_n[i-1]), replace = FALSE))
+z<-which(y %in% sample(y,(missing_n[i]-missing_n[i-1]), replace = FALSE))
 y_missing_integers[[i]]<-c(x,y_missing_integers[[i-1]])
 }
 
 #Assign NAs to the missing data in each list
 for(i in 1:length(missing_n)){
-  x<-y_miss[[i]]
-  x[y_missing_integers[[i]]]<-NA
-  y_miss[[i]]<-x
+  z<-y_miss[[i]]
+  z[y_missing_integers[[i]]]<-NA
+  z[1]<-y[1]
+  y_miss[[i]]<-z
 }
+
 
 #Check that each list has the right amount of missing data (or close to it...)
 miss<-sapply(y_miss, function(y) sum(length(which(is.na(y)))))
@@ -167,6 +188,7 @@ cat("
     real<lower = 0> sigma_proc; // process error
     real<lower = 0> sigma_obs; // observation error
     real<lower = 0, upper=1 > phi;  // auto-regressive parameter
+    real b0; // intercept
     real b1; // light parameter
     //real b2; // turb paramater
     }
@@ -180,20 +202,21 @@ cat("
     model {
     X[1]~normal(y[1],sigma_proc); //set initial state
     
-    for(i in 1:N){
-    y[i] ~ normal(X[i], sigma_obs); // observation model
-    }
+   
+    y ~ normal(X, sigma_obs); // observation model
+    
     
     for (t in 2:N){
-    X[t] ~ normal(phi*X[t-1]+b1*light[t], sigma_proc); // process model with unknown process error //regression model with AR errors
+    X[t] ~ normal(b0+phi*X[t-1]+b1*light[t], sigma_proc); // process model with unknown process error //regression model with AR errors
     
     }  
     
     // error priors
-    sigma_proc ~ normal(0, 2); 
-    sigma_obs ~ normal(0, 2);
+    sigma_proc ~ normal(0, 1); 
+    sigma_obs ~ normal(0, 1);
     
     // single parameters priors 
+    b0~normal(0, 5);
     b1~normal(0, 5);
   
     //Prior for AR coefficient needs to be reparameterized
@@ -206,7 +229,7 @@ generated quantities {
  x_rep[1]=normal_rng(y[1], 0.1);
  
  for (t in 2:N) {
- x_rep[t]=normal_rng(phi*X[t-1]+b1*light[t], sigma_proc);
+ x_rep[t]=normal_rng(b0+phi*X[t-1]+b1*light[t], sigma_proc);
  }
  for (t in 1:N) {
  y_rep[t]=normal_rng(x_rep[t], sigma_obs);
@@ -220,32 +243,33 @@ generated quantities {
 sink()
 closeAllConnections()
 
-fit <- vector("list",length(missing_n))
+fit.miss <- vector("list",length(missing_n))
 
-for(i in 1:length(missing_n)){
+for(i in 1:1){
 ##Load data
 data <- list(   N = N,
                 y_nmiss = unlist(y_nMiss[i]),
                 y_nobs = unlist(y_nObs[i]),
                 y_ind_mis = unlist(y_index_mis[i]),
                 y_ind_obs = unlist(y_index_obs[i]),
-                y_miss= y_miss[[i]],
+                y= unlist(y_miss[i]),
                 light=light)
 
 ##Run Stan
-fit[[i]]<- stan("ss_missing data.stan", data = data,  iter = 4000, chains = 4)
+fit.miss[[1]]<- stan("ss.stan", data = data,  iter = 4000, chains = 4, control=list(max_treedepth = 15))
 }
 
 ##Pull param estimates into list
 fit_summary_pars <- vector("list",length(missing_n))
 for (i in 1:length(missing_n)){
-   fit_summary_pars[[i]]<-(summary(fit[[i]], pars=c("sigma_proc","phi", "b1", "sigma_obs"), probs=c(0.05,.5,.95))$summary)
+   fit_summary_pars[[i]]<-(summary(fit.miss[[i]], pars=c("sigma_proc","phi", "b1", "sigma_obs"), probs=c(0.05,.5,.95))$summary)
    }
+fit_summary_pars[[1]]
 
 ##Unlist,cleanup, and add factors
 fit_summary<-as.data.frame(do.call(rbind.data.frame, fit_summary_pars)) #Unlist
 fit_summary$param<-rep(c("sigma_proc","phi", "b1", "sigma_obs"), times=length(missing_n) )#add parameter factor
-fit_summary$prop.missing<-rep(prop.miss, times=c(4,4,4,4,4,4,4)) #add prop of missing data
+fit_summary$prop.missing<-rep(prop.miss, times=c(4,4,4,4,4,4,4,4)) #add prop of missing data
 row.names(fit_summary)<-1:(length(missing_n)*4) #remove row names
 fit_summary$param<-as.factor(fit_summary$param) #make factor
 fit_summary$prop.missing<-as.factor(fit_summary$prop.missing) #make factor
@@ -284,17 +308,17 @@ print(fit[[2]], pars=c( "sigma_proc","phi", "b1", "sigma_obs" ))
 pairs(fit, pars=c("sigma_proc","phi", "b1", "sigma_obs"))
 
 ##HMC diagnostics
-rstan::check_hmc_diagnostics(fit[[1]])
+rstan::check_hmc_diagnostics(fit.miss[[1]])
 
 ##Traceplots
 
-traceplot(fit[[1]], pars=c("phi", "b1","sigma_proc", "sigma_obs"))
+traceplot(fit.miss[[1]], pars=c("phi", "b1","sigma_proc", "sigma_obs"))
 
 ##Posterior densities comopared to known parameters
-plot_sdo <- stan_dens(fit[[6]], pars="sigma_obs") + geom_vline(xintercept =sd.o)
-plot_sdp <- stan_dens(fit[[6]], pars="sigma_proc") + geom_vline(xintercept = sd.p)
-plot_phi <- stan_dens(fit[[6]], pars="phi") + geom_vline(xintercept = phi)
-plot_b1 <- stan_dens(fit[[6]], pars="b1") + geom_vline(xintercept = b1)+xlab("Light beta")
+plot_sdo <- stan_dens(fit.miss[[1]], pars="sigma_obs") + geom_vline(xintercept =sd.o)
+plot_sdp <- stan_dens(fit.miss[[1]], pars="sigma_proc") + geom_vline(xintercept = sd.p)
+plot_phi <- stan_dens(fit.miss[[1]], pars="phi") + geom_vline(xintercept = phi)
+plot_b1 <- stan_dens(fit.miss[[1]], pars="b1") + geom_vline(xintercept = b1)+xlab("Light beta")
 grid.arrange(plot_b1, plot_phi,plot_sdp,plot_sdo, nrow=2)
 
 ##Posterioir Predictive check and test statistic
@@ -349,5 +373,118 @@ ggplot(data=y_combined, aes(x=observed, y=estimated))+
   theme(axis.text.x=element_text(size=18,colour = "black"))
 
 
+###############################
+#####Multiple Imputations######
+###############################
+ts<-as.Date(time,format="%m/%d/%Y")
+
+data.amelia<- vector("list",length(missing_n))
+for(i in 1:length(missing_n)){
+y_miss1<-na_if(y_miss[[i]], -100)
+y_miss1[1]<-y[1]
+z<-as.data.frame(cbind(y_miss1, ts))
+z2<-amelia(z, m = 5, p2s=1, ts="ts", polytime = 2, emperi=.1*nrow(ts))
+summary(z2)
+data.amelia[[i]]<-z2
+}
+str(z2$imputations,1)
+
+fit.amelia <- vector("list",length(missing_n))
+
+for(i in 2:length(missing_n)){##two because the list holding the lists starts with a list of misc data that arent the estimates
+  for (g in 1:5){
+  ##Load data
+  data <- list(   N = N,
+                  y_miss= data.amelia[[2]]$imputations[[1]]$y_miss1,
+                  light=light)
+
+}
+}
+  ##Run Stan
+  fit.1[[i]]<- stan("ss_missing data.stan", data = data,  iter = 4000, chains = 4, control=list(max_treedepth = 15))
 
 
+##Stan Code
+
+sink("ss.stan")
+
+cat("
+    data {
+    int<lower=1> N; // Number of observations
+    vector[N] y; // Response variable, including missing values
+    vector[N] light; //light data
+   
+    }
+    
+    parameters {
+    vector[N] X; // latent state data
+    real<lower = 0> sigma_proc; // process error
+    real<lower = 0> sigma_obs; // observation error
+    real<lower = 0, upper=1 > phi;  // auto-regressive parameter
+    real b0; // intercept
+    real b1; // light parameter
+      }
+    
+    model {
+    X[1]~normal(y[1],0.01); //set initial state
+    
+   
+    y ~ normal(X, sigma_obs); // observation model
+    
+    
+    for (t in 2:N){
+    X[t] ~ normal(b0+phi*X[t-1]+b1*light[t], sigma_proc); // process model with unknown process error 
+    
+    }  
+    
+    // error priors
+    sigma_proc ~ normal(0, 1); 
+    sigma_obs ~ normal(0, 1);
+    
+    // single parameters priors 
+    b0~normal(0, 5);
+    b1~normal(0, 5);
+  
+    //Prior for AR coefficient needs to be reparameterized
+    phi~beta(1,1);
+    }
+ 
+generated quantities {
+ vector[N] x_rep; // replications from posterior predictive dist
+ vector [N] y_rep;
+ x_rep[1]=normal_rng(y[1], 0.1);
+ 
+  x_rep[1]=normal_rng(y[1], 0.1);
+ y_rep[1]=normal_rng(y[1], 0.1);
+ 
+ for (t in 2:N) {
+ x_rep[t]=normal_rng(b0+phi*X[t-1]+b1*light[t], sigma_proc);
+ y_rep[t]=normal_rng(x_rep[t], sigma_obs);
+ }
+ }
+ 
+    "
+    ,fill=TRUE)
+sink()
+closeAllConnections()
+
+#for(i in 1:1){
+  ##Load data
+  data <- list(   N = N,
+                  y= y,
+                  light=light)
+  
+  ##Run Stan
+  fit.miss<- stan("ss.stan", data = data,  iter = 1000, chains = 4)
+  ##Traceplots
+  
+  traceplot(fit.miss, pars=c("b0","b1", "phi", "sigma_proc", "sigma_obs"))
+  
+  ##Posterior densities comopared to known parameters
+  plot_sdo <- stan_dens(fit.miss, pars="sigma_obs") + geom_vline(xintercept =sd.o)
+  plot_sdp <- stan_dens(fit.miss, pars="sigma_proc") + geom_vline(xintercept = sd.p)
+  plot_phi <- stan_dens(fit.miss, pars="phi") + geom_vline(xintercept = phi)
+  plot_b1 <- stan_dens(fit.miss, pars="b1") + geom_vline(xintercept = b1)+xlab("Light beta")
+  plot_b0 <- stan_dens(fit.miss, pars="b0") + geom_vline(xintercept = b0)+xlab("Intercept")
+  grid.arrange( plot_b0, plot_phi,plot_sdp,plot_sdo, nrow=2)
+  
