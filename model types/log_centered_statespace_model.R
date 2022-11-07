4b4e1e7cad2a5e9751512c9003301347376ba8cb
Alice Carter <alicecarter05@gmail.com>
2022-11-07 15:06:22 -0700

    modified GPP stan model to fix observation error
    Former-commit-id: 783a0e09b371886150d9bf71cb1275f0e2f1560a

--- model types/log_centered_statespace_model.R
+++ model types/log_centered_statespace_model.R
@@ -43,22 +43,22 @@ tt <- 1:N
 ##GPP based on time-series model with known parameters
 # GPP_t = b0 + phi*GPP_t-1 + b1*L + proc_error
 # GPP_obs = GPP + obs_error
-b0 <- 1      # intercept
-b1 <- 0.6   # light coefficient
-phi <- 0.4   # ar1 coefficient
-sigma_proc <- 0.1 # ~10% error
-sigma_obs <- 0.1
+b0 <- 3      # intercept
+b1 <- 1   # light coefficient
+phi <- 0.5   # ar1 coefficient
+sigma_proc <- 0.3 # ~5% error
+sigma_obs <- 0.1  # fixed in model
 
 # Expected value based on parameters:  ~10.4
-EV <- exp((b0*(1-phi) + b1 * light.rel[1])/(1-phi))
-GPP <- numeric(N)  # GPP state variable
-GPP[1] <- EV
+GPP <- vector(mode = 'double', length = N)  # GPP state variable
+GPP[1] <- b0 + b1 * light.rel[1] + 
+                rnorm(1, 0, sd = sigma_proc/sqrt(1-phi^2))
 
 # Observed GPP: add observation error on a linear, not a log scale
 P_obs = rnorm(1, GPP[1], sigma_obs)
 for (i in 2:N){
-  GPP[i] <- exp(b0*(1-phi) + b1 * light.rel[i] + phi * log(GPP[i-1]) + 
-                  rnorm(1, 0, sigma_proc))
+  GPP[i] <- b0 + b1 * light.rel[i] + phi * (GPP[i-1] - b0) + 
+                  rnorm(1, 0, sigma_proc)
   P_obs[i] = rnorm(1, GPP[i], sigma_obs)
 }
 
@@ -70,65 +70,15 @@ legend('topright', c('state', 'observations'), pch = c(NA, 1), lty = c(1, 0),
 ##Bind simulated data and time
 y_full <- as.data.frame(cbind(GPP, P_obs, doy = as.factor(tt)))
 
-#Stan model
-sink("ss_light_log_cent.stan")
 
-cat("
-
-/*----------------------- Data --------------------------*/
-  data {
-    int N; // Length of state and observation time series
-    vector[N] P_obs; // observed GPP
-    vector[N] light;
-    real logP_0; // log initial value for GPP
-  }
-
-/*----------------------- Parameters --------------------------*/
-
-  parameters {
-    vector <lower = 0> [N] logGPP; // log latent variable - true GPP
-    real<lower=0> sdp; // Standard deviation of the process equation
-    real<lower=0> sdo; // Standard deviation of the observation equation
-    real b0; // intercept
-    real b1; // light coefficient
-    real<lower = 0, upper=1 > phi; // Auto-regressive parameter
-  }
-
-/*----------------------- Model --------------------------*/
-
-  model {
-    // Prior distributions
-    phi ~ beta(1,1);
-    b0 ~ normal(0, 5);
-    b1 ~ normal(0, 5);
-    sdp ~ normal(0,1);
-    sdo ~ normal(0,0.5);
-
-    // Distribution for the zero and first state
-    logGPP[1] ~ normal(logP_0, sdp);
-
-    // Distributions for states
-    for(i in 2:N){
-       logGPP[i] ~ normal(b0*(1-phi) + logGPP[i-1]*phi + light[i]*b1, sdp); // process model with error
-        }
-
-    // Observation error model
-    P_obs ~ normal(exp(logGPP), sdo);
-
-      }
-    
-    " ,fill=TRUE)
-
-sink()
-closeAllConnections()
-
+# model file: "model types/fixed_oi_light_centered.stan"
 #Prep model
-model <- "ss_light_log_cent.stan"
+model <- "model types/fixed_oi_light_centered.stan"
 model <- stan_model(model)
 
 #Create data object
 data <- list(N = length(P_obs), P_obs = P_obs,
-             logP_0 = log(P_obs[1]), light=light.rel)
+             sdo = sigma_obs, light=light.rel)
 
 #Run Stan
 fit <- rstan::sampling(object=model, data = data,  
@@ -136,26 +86,21 @@ fit <- rstan::sampling(object=model, data = data,
 
 ##Print and extract
 fit_extract <- rstan::extract(fit)
-print(fit, pars=c( "sdo","phi", "sdp", "b0","b1" ))
-pairs(fit, pars=c("sdo","phi", "sdp","b0","b1","lp__"))
+print(fit, pars=c("phi", "sdp", "beta" ))
+pairs(fit, pars=c("phi", "sdp","beta","lp__"))
 
 ##HMC diagnostics
 rstan::check_hmc_diagnostics(fit)
 
 ##Traceplots
-traceplot(fit, pars=c("sdo","phi", "sdp", "b1", "b0"))
+traceplot(fit, pars=c("phi", "sdp", "beta"))
 
 ##Plot density plots
-plot_sdo <- stan_dens(fit, pars="sdo") + geom_vline(xintercept = sigma_obs)
 plot_sdp <- stan_dens(fit, pars="sdp") + geom_vline(xintercept = sigma_proc)
 plot_phi <- stan_dens(fit, pars="phi") + geom_vline(xintercept = phi)
-plot_b0 <- stan_dens(fit, pars="b0") + geom_vline(xintercept = b0) +
+plot_b0 <- stan_dens(fit, pars="beta[1]") + geom_vline(xintercept = b0) +
   xlab("Intercept")
-plot_b1 <- stan_dens(fit, pars="b1") + geom_vline(xintercept = b1) +
+plot_b1 <- stan_dens(fit, pars="beta[2]") + geom_vline(xintercept = b1) +
   xlab("Light beta")
-gridExtra::grid.arrange(plot_b0, plot_b1, plot_phi, plot_sdp, plot_sdo, nrow=2)
+gridExtra::grid.arrange(plot_b0, plot_b1, plot_phi, plot_sdp, nrow=2)
 
-# I have found that recovering observation error is really difficult in a log
-# model, no matter what value is chosen. I think that while incorporating this
-# error structure does provide a more realistic description of the data, it
-# probably isn't the move for the missing data paper.
