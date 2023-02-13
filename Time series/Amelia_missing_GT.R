31a97f1696a54172ac4a8f50188ce6ca1f59b36a
Melissa <mdesierv@uwyo.edu>
2023-02-13 15:40:18 -0700

    playing around with multiple imputation
    Former-commit-id: 96549c9889fdd3c13a91f71dd318c8a46a7cbf2e

--- Time series/Amelia_missing_GT.R
+++ Time series/Amelia_missing_GT.R
@@ -27,6 +27,10 @@ library(performance)
 library(here)
 library(tseries)
 
+#helpful resource for MI##
+#https://thomasleeper.com/Rcourse/Tutorials/mi.html#
+
+
 ##upload the great tit population data##
 
 titpop<- read.csv(here("data/Wytham_tits.csv"), header=T, check.names=FALSE)
@@ -72,12 +76,16 @@ amelia1[["imputations"]][["imp1"]][["Time"]]
 
 ####
 
-## for loop to do an NLS model for AMELIA imputation###
+## for loop to do an NLS model from Great Tit for AMELIA imputation###
 
 # make empty lists to hold the output
 
 amelia_list <- replicate(length(5),rep(NA, times = nrow(titpopdf1)) , simplify = FALSE)
 amelia_list_t1 <- replicate(length(5),rep(NA, times = nrow(titpopdf1)) , simplify = FALSE)
+
+df1<-data.frame(matrix(NA, nrow=nrow(titpopdf1), ncol=2))
+
+listofdf<-list(df1, df1, df1, df1, df1)
 
 modelOutput_list <- replicate(length(5), rep(NULL), simplify = FALSE)
 
@@ -86,10 +94,34 @@ modelOutput_list <- replicate(length(5), rep(NULL), simplify = FALSE)
 for(i in 1:5){
   amelia_list [[i]] <-amelia1[["imputations"]][[i]]$Broods
   amelia_list_t1[[i]]<- Lag(amelia_list [[i]],-1)
-  list_df=list(data.frame(Broods=unlist(amelia_list[[i]]),popplus1=unlist(amelia_list_t1[[i]]))) ##don't have this right##
-  }
+  listofdf[[i]]<-data.frame(Broods=unlist(amelia_list [[i]]),popplus1=unlist(amelia_list_t1[[i]]))
+  
+  ## fit an NLS model to each AMELIA dataset ##
+  Rickermissing_i <-nls(popplus1~Broods*exp(r-alpha*Broods),start=list(r=0.1, alpha= 1/200), data=listofdf[[i]])
+  modelOutput_list[[i]] <- summary(Rickermissing_i)$coefficients
+ }
 
 
+#### another way to do this part using an apply function###
+
+#lm.amelia.out <- lapply(imp.amelia$imputations, function(i) lm(y ~ x1 + x2, 
+                                                               #data = i))
+##model output list into a dataframe to average..##
+
+
+coefdf<-do.call(rbind, modelOutput_list)
+params <- rownames(coefdf)
+coefdf1<-cbind(params, as.data.frame(coefdf[,1:2]))
+rownames(coefdf1)<-NULL
+
+est1<-coefdf1[,1:2]
+se1<-coefdf1[,c(1,3)]
+
+est2<-pivot_wider(est1)
+##need to do model averaging across AMELIA impuations##
+
+## Uses Rubin's rules for combining a set of results from multiply imputed datasets to reflect the average result, #
+mi.meld(coefs.amelia, ses.amelia)
 
 ####
 
