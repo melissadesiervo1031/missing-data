ece7e53333f7cd26624a443a8575defd4d10d890
Melissa <mdesierv@uwyo.edu>
2023-04-28 11:45:18 -0600

    update from Alica S.
    Former-commit-id: 8f2b2d8b6f0355650c7341394d86cf2c3b437067

--- missing_data_functions.R
+++ missing_data_functions.R
@@ -59,36 +59,51 @@ makeMissing <- function(timeSeries, # a time series in vector format (a single v
                      "% missing data, because a single gap results in a greater proportion of missing data than indicated for this value of the 'propMiss' argument")) 
         next      }     
       
+      # make the length of the gap slightly random (draw from a poisson distribution) %%I think this is the right distribution to use?      
+      # get a vector of possible gap sizes, then pick the first 
+      chunkSize_TEMP <- rpois(n = 100, lambda = chunkSize_f)        
+      # make sure that chunks are at least 1  unit long
+      chunkSize_TEMP[chunkSize_TEMP==0] <- 1
+      # get the correct number of chunks that correspond to the amount of missing 
+      # data that the propMiss_f[i] argument specificies
+      gapsNeeded <- propMiss_f[i]*length(timeSeries)
+      
+      chunkMat <- matrix(NA, nrow = 100, ncol = 100)
+      for (k in 1:length(chunkSize_TEMP)) {
+        chunkMat[,k] <- c(chunkSize_TEMP[1:k], rep(NA, length.out = 100-k))
+      }
+      # find out how many of the chunks we need to get the amount of missing data we want
+      chunkSize_i <- chunkSize_TEMP[1:which.min(abs(colSums(chunkMat, na.rm = TRUE) - gapsNeeded))]
+      
       # calculate the shape parameter (the number of gaps that will need to
       # exist to get the specified proportion of missing data in propMiss_f[i]     
-      n_events_i <- (propMiss_f[i] * length(timeSeries))/chunkSize_f      # calculate the scale parameter (what is the mean time between events that
+      n_events_i <- length(chunkSize_i)  
       
+      # calculate the scale parameter (what is the mean time between events that
       # needs to exist to get the specified proportion of missing data in propMiss_f[i]   
-      beta_i <- (length(timeSeries)-(n_events_i * chunkSize_f))/n_events_i            
+      beta_i <- round((length(timeSeries) - sum(chunkSize_i))/n_events_i)         
       
       # given the average distance between chunks calculated above, when should       
       # the next gap occur? (returns the the number of intervals needed for the number of events for this i)      
-      intervals_i <- round(rgamma(n = n_events_i, shape = 1, scale = beta_i))            
-      
-      # make the length of the gap slightly random (draw from a poisson distribution) %%I think this is the right distribution to use?      
-      chunkSize_i <- rpois(n = n_events_i, lambda = chunkSize_f)        
-      # make sure that chunks are at least 1  unit long
-      chunkSize_i[chunkSize_i==0] <- 1
+      # intervals are random, but make it so that the length of the events/iterations can't be longer than the length of the time series
+      repeat {
+        intervals_i <- round(rgamma(n = n_events_i, shape = 1, scale = beta_i))  
+        if (sum(intervals_i, chunkSize_i) <= length(timeSeries)) {
+          break
+        }
+      }          
       
       ## now replace values in the timeSeries according in chunks of size chunkSize_i along intervals intervals_i      
       # get the indices of the starts and ends of each gap      
-      indices_temp <- c(rbind(intervals_i, (chunkSize_i-1)))      
-      indices_i <- c(indices_temp[1])      
+      indices_temp <- c(rbind((intervals_i+1), (chunkSize_i-1)))      
+      indices_j <- c(indices_temp[1])      
       for (j in 2:length(indices_temp)) {        
-        indices_i <- append(indices_i, values = indices_i[j-1] + indices_temp[j])      
+        indices_j <- append(indices_j, values = indices_j[j-1] + indices_temp[j])      
       }      
       
-      # if the indices are longer than the length of the timeSeries, then "recycle" them to start again in the time series      
-      indices_i[indices_i > length(timeSeries)] <- indices_i[indices_i > length(timeSeries)] - length(timeSeries)      
-      
       indices_seq <- NULL      
-      for (k in seq(from = 2, to = length(indices_i), by = 2)) {        
-        indices_seq <- append(indices_seq, indices_i[k-1]:indices_i[k])      
+      for (k in seq(from = 2, to = length(indices_j), by = 2)) {        
+        indices_seq <- append(indices_seq, indices_j[k-1]:indices_j[k])      
       }          
       
       ## store the output
@@ -192,7 +207,7 @@ makeMissing <- function(timeSeries, # a time series in vector format (a single v
                                         
                                         values = NA))    
     
-  
+    
     
     names(missingDat_list) <- paste0("propMissAct_",  
                                      lapply(X = missingDat_list, 
@@ -201,7 +216,7 @@ makeMissing <- function(timeSeries, # a time series in vector format (a single v
   return(missingDat_list)
 }
 
-# testing -----------------------------------------------------------------
+# testing for functionality -----------------------------------------------------------------
 ## load example data
 ## 1000 simulated AR(1) time series with 2 covariates to mimic the basic structure 
 #of the GPP dataset. Each time series is stored in a list and named `y`. The 
@@ -215,12 +230,110 @@ makeMissing <- function(timeSeries, # a time series in vector format (a single v
 # stored in a list and named `y`. The parameters used to simulate the data are 
 # stored in a sublist called `sim_params` and include `r`, the intrinsic growth 
 # rate, and `alpha`, the intraspecific competition coefficient.
-#ricker <- read_rds("./data/ricker_0miss_datasets.rds")
+ricker <- readRDS("./data/ricker_0miss_datasets.rds")
+
 ## testing with a subset of the ricker data
-
-#makeMissing(timeSeries = ricker[[1]]$y, typeMissing = "random", propMiss = c(.5, .05))
-#makeMissing(timeSeries = ricker[[1]]$y, typeMissing = "evenChunks", propMiss = c(.5, .05), chunkSize = 3)
-
+makeMissing(timeSeries = ricker[[1]]$y, typeMissing = "random", propMiss = c(.5, .05))
+makeMissing(timeSeries = ricker[[1]]$y, typeMissing = "evenChunks", propMiss = c(.5, .05), chunkSize = 3)
 
 ## testing w/ all of the ricker data list
-#lapply(X = ricker, FUN = function(X) makeMissing(timeSeries = X$y, typeMissing = "random"))
+lapply(X = ricker, FUN = function(X) makeMissing(timeSeries = X$y, typeMissing = "random"))
+
+
+# Testing for MCAR vs MNAR ------------------------------------------------
+library(ggpubr)
+
+# use the simulated gaussian AR1 data
+gausSim <- readRDS("./data/gauss_ar1_0miss_datasets.rds")
+# for now, get just the first dataset
+gausSim <- data.frame("y" = gausSim[[1]]$y, 
+                      "time" = 1:length(gausSim[[1]]$y))
+
+# get 40% missing completely at random 
+gausSim$y_randMiss <- as.vector(unlist(makeMissing(timeSeries = gausSim$y, typeMissing = "random", propMiss = .4)))
+
+# get 40% missing in evenly spaced chunks
+gausSim$y_evenChunkMiss <- as.vector(unlist(makeMissing(timeSeries = gausSim$y, typeMissing = "evenChunks", propMiss = .4, chunkSize = 5)))
+
+# get 40% missing in randomly spaced chunks
+gausSim$y_randChunkMiss <- as.vector(unlist(makeMissing(timeSeries = gausSim$y, typeMissing = "randChunks", propMiss = .4, chunkSize = 5)))
+
+# get 40% missing in minmax of data
+gausSim$y_minMaxMiss <- as.vector(unlist(makeMissing(timeSeries = gausSim$y, typeMissing = "minMax", propMiss = .4)))
+
+# make histograms like Dusty made (looking at histogram of values in the missing
+# data time series vs. expected distribution from the AR1 process)
+# no missing data
+noMiss_line <- ggplot(data = gausSim, aes(x = time, y = y)) + 
+  geom_line() + 
+  geom_point(size = 1) +
+  ggtitle("No Missing Data") + 
+  theme_classic()
+
+noMiss_hist <- ggplot() + 
+  geom_histogram(data = gausSim, aes(y, after_stat(density)), fill = "grey", color = "darkgrey") + 
+  geom_line(aes(y = dnorm(seq(-5,7,.1), mean = mean(gausSim$y), sd = sd(gausSim$y)), 
+                x = seq(-5,7,.1)), color = "blue") + 
+  theme_classic()
+
+# randomly missing
+randMiss_line <- ggplot(data = gausSim, aes(x = time, y = y_randMiss)) + 
+  geom_line() + 
+  geom_point(size = 1) +
+  ggtitle("Missing Completely At Random") + 
+  theme_classic()
+
+randMiss_hist <- ggplot() + 
+  geom_histogram(data = gausSim, aes(y_randMiss, after_stat(density)), fill = "grey", color = "darkgrey") + 
+  geom_line(aes(y = dnorm(seq(-5,7,.1), mean = mean(gausSim$y), sd = sd(gausSim$y)), 
+                x = seq(-5,7,.1)), color = "blue") + 
+  theme_classic()
+
+# missing evenly spaced chunks
+evenChunkMiss_line <- ggplot(data = gausSim, aes(x = time, y = y_evenChunkMiss)) + 
+  geom_line() + 
+  geom_point(size = 1) +
+  ggtitle("Missing Even Chunks") + 
+  theme_classic()
+
+evenChunkMiss_hist <- ggplot() + 
+  geom_histogram(data = gausSim, aes(y_evenChunkMiss, after_stat(density)), fill = "grey", color = "darkgrey") + 
+  geom_line(aes(y = dnorm(seq(-5,7,.1), mean = mean(gausSim$y), sd = sd(gausSim$y)), 
+                x = seq(-5,7,.1)), color = "blue") + 
+  theme_classic()
+
+# missing randomly spaced chunks
+randChunkMiss_line <- ggplot(data = gausSim, aes(x = time, y = y_randChunkMiss)) + 
+  geom_line() + 
+  geom_point(size = 1) +
+  ggtitle("Missing Randomly Spaced Chunks") + 
+  theme_classic()
+
+randChunkMiss_hist <- ggplot() + 
+  geom_histogram(data = gausSim, aes(y_randChunkMiss, after_stat(density)), fill = "grey", color = "darkgrey") + 
+  geom_line(aes(y = dnorm(seq(-5,7,.1), mean = mean(gausSim$y), sd = sd(gausSim$y)), 
+                x = seq(-5,7,.1)), color = "blue") + 
+  theme_classic()
+
+# missing at min and max
+minMaxMiss_line <- ggplot(data = gausSim, aes(x = time, y = y_minMaxMiss)) + 
+  geom_line() + 
+  geom_point(size = 1) +
+  ggtitle("Missing Min and Max") + 
+  ylim(c(-2.5,5)) +
+  theme_classic()
+
+minMaxMiss_hist <- ggplot() + 
+  geom_histogram(data = gausSim, aes(y_minMaxMiss, after_stat(density)), fill = "grey", color = "darkgrey") + 
+  geom_line(aes(y = dnorm(seq(-5,7,.1), mean = mean(gausSim$y), sd = sd(gausSim$y)), 
+                x = seq(-5,7,.1)), color = "blue") + 
+  theme_classic()
+
+ggarrange(noMiss_line, noMiss_hist, 
+          randMiss_line, randMiss_hist, 
+          evenChunkMiss_line, evenChunkMiss_hist,
+          randChunkMiss_line, randChunkMiss_hist,
+          minMaxMiss_line, minMaxMiss_hist,
+          widths = c(.75,.25, .75,.25, .75,.25, .75,.25, .75,.25), 
+          ncol = 2, 
+          nrow = 5)
