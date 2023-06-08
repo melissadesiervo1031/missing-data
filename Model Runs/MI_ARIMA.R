7cc44a136572f4f1304cb32916b44e42a633a672
Melissa <mdesierv@uwyo.edu>
2023-06-08 13:57:25 -0600

    updated the ARIMA funcions and GPP plotting coe

--- Model Runs/MI_ARIMA.R
+++ Model Runs/MI_ARIMA.R
@@ -8,93 +8,21 @@ library(nlme)
 library(tidyverse)
 library(lubridate)
 
-
-# Reference A. Stears' code with helpful function for removing data
-# makeMissing()
-source("Functions/missing_data_functions.R")
-
-
-#### Read in the missing dataframes that Alice S. made #####
-###### Pull out the first in 1000 nested lists for this code ### (Eventually loop over all the lists)
-
-
-###### Pull out the first in 1000 nested lists for this code ### (Eventually loop over all the lists)
-
-
-gauss_sim_MAR_datasets <- readRDS(here("data/Missingdatasets/gauss_sim_randMiss.rds"))
-
-
-##For nested list of GPP datasets with increasing MAR data add back in the date column and the covariates## 
-
-
-GPP_sim_MAR<- gauss_sim_MAR_datasets [[1]][["y"]]
-
-sim1<-gauss_sim_MAR_datasets [[1]][["y"]][["y_noMiss"]]
-
-covariates<-gauss_sim_MAR_datasets[[1]][["sim_params"]][["X"]]
-
-covariatesX<-as.matrix(covariates[,2:3])
-
-days<-seq(1, 365)
-
-
-sim1df<-as.data.frame(cbind(days=days, GPP=sim1, light=covariates[,2], discharge=covariates[,3]))
-
-GPP_sim_MAR_2 <-lapply(X = GPP_sim_MAR, FUN = function(X)   cbind.data.frame(GPP=X, days=sim1df$days, light = sim1df$light, discharge = sim1df$discharge))
-
-
-#### MISSING COMPLETELY AT RANDOM (MCAR) ######
-
-################################################## #########################################################
-# MULTIPLE IMPUTATIONS W/ AMELIA     ARIMA TO SOLVE FOR PARAMETER ESTIMATES       AVERAGE ESTIMATES
+################################################# #########################################################
+# MISSING AT RANDOM
 ############################################################################################################
 
-amelia1sim <-lapply(X = GPP_sim_MAR_2 , FUN = function(X)   amelia(X, ts="days", m=5, lags="GPP")) ## lags by 1 day ##
 
+gauss_sim_MAR_datasets <- readRDS("data/Missingdatasets/gauss_sim_randMiss.rds")
+GPP_sim_MAR<- gauss_sim_MAR_datasets [[1]]
 
-##nested list of dataframes that just has the imputations###
-amelias11sim<-map(amelia1sim , ~.[["imputations"]])
-
-
-###
-##matrix of covariates##
-X = matrix(c(sim1df$light,  sim1df$discharge), ncol = 2)
-
-##forloop## gives us the model parameters and errors for all the ARIMA models on imputed datasets
-
-modelparamlistsim=list()
-modelerrorlistsim=list()
-for (i in seq_along(amelias11sim)) {
-  a=list()
-  aa=list()
-  for (j in seq_along(amelias11sim[[i]])) {
-    tempobj=Arima(amelias11sim[[i]][[j]]$GPP, order = c(1,0,0), xreg = X)
-    arimacoefs<-tempobj$coef
-    arimases<-sqrt(diag(vcov(tempobj)))
-    name <- paste('imp',seq_along((amelias11sim)[[i]])[[j]],sep='')
-    a[[name]] <- arimacoefs
-    aa[[name]]<-arimases
-  }
-  name1 <- names(amelias11sim)[[i]]
-  modelparamlistsim[[name1]] <- a
-  modelerrorlistsim[[name1]] <- aa
-}
-
-modelparamlistsim
-modelerrorlistsim
-
-
-### Averages the models together back to 1 model per missing data prop ##
-
-listcoefsessim<-mapply(function(X,Y) {
-  list(mi.meld(data.frame(X), data.frame(Y), byrow=FALSE))
-}, X=modelparamlistsim, Y=modelerrorlistsim)
+arima_mi_MAR<- fit_arima_MI(GPP_sim_MAR$y,GPP_sim_MAR$sim_params, imputationsnum=5)
 
 ##pulls out parameters and ses ##
 
-paramlistsim<-map(listcoefsessim , ~.["q.mi"])
-selistsim<-map(listcoefsessim , ~.["se.mi"])
+paramlistsim<-arima_mi_MAR[[1]]
 
+selistsim<-arima_mi_MAR[[2]]
 
 avgparamdf <- map_df(paramlistsim, ~as.data.frame(.x), .id="missingprop")
 avglSEdf <- map_df(selistsim, ~as.data.frame(.x), .id="missingprop")
@@ -122,86 +50,16 @@ paramMIlong2<-merge(paramMIlong,paramMISElong)
 
 ############ MISSING NOT AT RANDOM MNAR ##############################
 
+gauss_sim_MNAR_datasets <- readRDS("data/Missingdatasets/gauss_sim_minMaxMiss.rds")
+GPP_sim_MNAR<- gauss_sim_MNAR_datasets [[1]]
 
-###### Pull out the first in 1000 nested lists for this code ### (Eventually loop over all the lists)
-
-gauss_sim_MNAR_datasets <- readRDS(here("data/Missingdatasets/gauss_sim_minMaxMiss.rds"))
-
-##For nested list of GPP datasets with increasing MNAR data add back in the date column and the covariates## 
-
-GPP_sim_MNAR<- gauss_sim_MNAR_datasets [[1]][["y"]]
-
-GPP_sim_MNAR_2 <-lapply(X = GPP_sim_MNAR, FUN = function(X)   cbind.data.frame(GPP=X, days=sim1df$days, light = sim1df$light, discharge = sim1df$discharge))
-
-
-################################################## #########################################################
-# MULTIPLE IMPUTATIONS W/ AMELIA     ARIMA TO SOLVE FOR PARAMETER ESTIMATES       AVERAGE ESTIMATES
-############################################################################################################
-
-amelia1simMNAR <-lapply(X = GPP_sim_MNAR_2 , FUN = function(X)   amelia(X, ts="days", m=5, lags="GPP")) ## lags by 1 day ##
-
-
-##nested list of dataframes that just has the imputations###
-amelias11simMNAR<-map(amelia1simMNAR , ~.[["imputations"]])
-
-
-
-###### Pull out the first in 1000 nested lists for this code ### (Eventually loop over all the lists)
-
-gauss_sim_MNAR_datasets <- readRDS(here("data/Missingdatasets/gauss_sim_minMaxMiss.rds"))
-
-## no missing###
-
-sim1MNAR<-gauss_sim_MNAR_datasets [[1]][["y"]][["y_noMiss"]]
-
-covariates<-gauss_sim_MNAR_datasets[[1]][["sim_params"]][["X"]]
-
-covariatesX<-as.matrix(covariates[,2:3])
-
-days<-seq(1, 365)
-
-sim1dfMNAR<-as.data.frame(cbind(days=days, GPP=sim1MNAR, light=covariates[,2], discharge=covariates[,3]))
-
-
-
-###
-##matrix of covariates##
-X = matrix(c(sim1dfMNAR$light,  sim1dfMNAR$discharge), ncol = 2)
-
-##forloop## gives us the model parameters and errors for all the ARIMA models on imputed datasets
-
-modelparamlistsimMNAR=list()
-modelerrorlistsimMNAR=list()
-for (i in seq_along(amelias11simMNAR)) {
-  a=list()
-  aa=list()
-  for (j in seq_along(amelias11simMNAR[[i]])) {
-    tempobj=Arima(amelias11simMNAR[[i]][[j]]$GPP, order = c(1,0,0), xreg = X)
-    arimacoefs<-tempobj$coef
-    arimases<-sqrt(diag(vcov(tempobj)))
-    name <- paste('imp',seq_along((amelias11simMNAR)[[i]])[[j]],sep='')
-    a[[name]] <- arimacoefs
-    aa[[name]]<-arimases
-  }
-  name1 <- names(amelias11simMNAR)[[i]]
-  modelparamlistsimMNAR[[name1]] <- a
-  modelerrorlistsimMNAR[[name1]] <- aa
-}
-
-modelparamlistsimMNAR
-modelerrorlistsimMNAR
-
-
-### Averages the models together back to 1 model per missing data prop ##
-
-listcoefsessimMNAR<-mapply(function(X,Y) {
-  list(mi.meld(data.frame(X), data.frame(Y), byrow=FALSE))
-}, X=modelparamlistsimMNAR, Y=modelerrorlistsimMNAR)
+arima_mi_MNAR<- fit_arima_MI(GPP_sim_MNAR$y,GPP_sim_MNAR$sim_params, imputationsnum=5)
 
 ##pulls out parameters and ses ##
 
-paramlistsimMNAR<-map(listcoefsessimMNAR , ~.["q.mi"])
-selistsimMNAR<-map(listcoefsessimMNAR , ~.["se.mi"])
+paramlistsimMNAR<-arima_mi_MNAR[[1]]
+
+selistsimMNAR<-arima_mi_MNAR[[2]]
 
 
 avgparamdfMNAR <- map_df(paramlistsimMNAR, ~as.data.frame(.x), .id="missingprop")
@@ -226,3 +84,4 @@ paramMIlongMNAR <- gather(MIdfMNAR2, param, value, ar1:discharge, factor_key=TRU
 paramMISElongMNAR <- gather(MISEdfMNAR2, param, SE, ar1:discharge, factor_key=TRUE)
 
 paramMIlongMNAR2<-merge(paramMIlongMNAR,paramMISElongMNAR)
+
