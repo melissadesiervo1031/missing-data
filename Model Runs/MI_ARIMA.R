1c0e0a9464d470e1ac5557318f11d844c19e98c8
Melissa <mdesierv@uwyo.edu>
2023-07-18 17:18:28 -0600

    updated modeul runs to include autocor
    Former-commit-id: d56fa094bd991d74733c2b8b6ad643d3829f4f0c

--- Model Runs/MI_ARIMA.R
+++ Model Runs/MI_ARIMA.R
@@ -9,11 +9,11 @@ library(tidyverse)
 library(lubridate)
 
 ################################################# #########################################################
-# MISSING AT RANDOM
+# MISSING AT RANDOM low auto
 ############################################################################################################
 
 
-gauss_sim_MAR_datasets <- readRDS("data/Missingdatasets/gauss_sim_randMiss.rds")
+gauss_sim_MAR_datasets <- readRDS("data/Missingdatasets/gauss_sim_randMiss_autoCorr_10.rds")
 GPP_sim_MAR<- gauss_sim_MAR_datasets [[1]]
 
 arima_mi_MAR<- fit_arima_MI(GPP_sim_MAR$y,GPP_sim_MAR$sim_params, imputationsnum=5)
@@ -24,28 +24,76 @@ paramlistsim<-arima_mi_MAR[[1]]
 
 selistsim<-arima_mi_MAR[[2]]
 
-avgparamdf <- map_df(paramlistsim, ~as.data.frame(.x), .id="missingprop")
-avglSEdf <- map_df(selistsim, ~as.data.frame(.x), .id="missingprop")
-
-missingprop<-seq(from=0.00, to =0.95, by=0.05)
-
-avgparamdf2<-avgparamdf %>% dplyr::rename(ar1=q.mi.ar1, intercept=q.mi.intercept, light=q.mi.xreg1, discharge=q.mi.xreg2) %>% select(ar1, intercept, light, discharge) %>% mutate(type="Multiple imputations")
-
-avglSEdf2<-avglSEdf  %>% dplyr::rename(ar1=se.mi.ar1, intercept=se.mi.intercept, light=se.mi.xreg1, discharge=se.mi.xreg2) %>% select(ar1, intercept, light, discharge) %>% mutate(type="Multiple imputations")
+avgparamdf <- map_df(paramlistsim, ~as.data.frame(.x), .id="missingprop_autocor")
+avglSEdf <- map_df(selistsim, ~as.data.frame(.x), .id="missingprop_autocor")
 
 
-missingprop<-seq(0.00, 0.95, by=0.05)
+avgparamdf2<-avgparamdf %>% dplyr::rename(ar1=q.mi.ar1, intercept=q.mi.intercept, light=q.mi.xreg1, discharge=q.mi.xreg2) %>%  separate(missingprop_autocor, into = c('missingprop1', 'autoCorr1'), sep = 16) %>% select(missingprop1, autoCorr1, ar1, intercept, light, discharge)  %>% mutate(missingness="MAR low auto") %>% mutate(type="Multiple imputations")
 
-MIdf2<-cbind(missingprop, avgparamdf2)
-
-MISEdf2<-cbind(missingprop, avglSEdf2)
+avglSEdf2<-avglSEdf  %>% dplyr::rename(ar1=se.mi.ar1, intercept=se.mi.intercept, light=se.mi.xreg1, discharge=se.mi.xreg2) %>%  separate(missingprop_autocor, into = c('missingprop1', 'autoCorr1'), sep = 16) %>% select(missingprop1, autoCorr1, ar1, intercept, light, discharge)   %>% mutate(missingness="MAR low auto") %>% mutate(type="Multiple imputations")
 
 
-paramMIlong <- gather(MIdf2, param, value, ar1:discharge, factor_key=TRUE)
+## pull out missing prop and autocorr values ##
 
-paramMISElong <- gather(MISEdf2, param, SE, ar1:discharge, factor_key=TRUE)
+avgparamdf2$missingprop<-as.numeric(str_extract(avgparamdf2$missingprop1, "\\d+\\.*\\d*"))
+
+avgparamdf2$autocorr<-as.numeric(str_extract(avgparamdf2$autoCorr1, "\\d+\\.*\\d*"))
+
+avglSEdf2$missingprop<-as.numeric(str_extract(avglSEdf2$missingprop1, "\\d+\\.*\\d*"))
+
+avglSEdf2$autocorr<-as.numeric(str_extract(avglSEdf2$autoCorr1, "\\d+\\.*\\d*"))
+
+
+
+paramMIlong <- gather(avgparamdf2, param, value, ar1:discharge, factor_key=TRUE)
+
+paramMISElong <- gather(avglSEdf2, param, SE, ar1:discharge, factor_key=TRUE)
 
 paramMIlong2<-merge(paramMIlong,paramMISElong)
+
+
+################################################# #########################################################
+# MISSING AT RANDOM high auto
+############################################################################################################
+
+
+gauss_sim_MAR_datasets_high <- readRDS("data/Missingdatasets/gauss_sim_randMiss_autoCorr_90.rds")
+GPP_sim_MAR_high<- gauss_sim_MAR_datasets_high [[1]]
+
+arima_mi_MAR_high<- fit_arima_MI(GPP_sim_MAR_high$y,GPP_sim_MAR_high$sim_params, imputationsnum=5)
+
+##pulls out parameters and ses ##
+
+paramlistsim_high<-arima_mi_MAR_high[[1]]
+
+selistsim_high<-arima_mi_MAR_high[[2]]
+
+avgparamdf_high <- map_df(paramlistsim_high, ~as.data.frame(.x), .id="missingprop_autocor")
+avglSEdf_high <- map_df(selistsim_high, ~as.data.frame(.x), .id="missingprop_autocor")
+
+
+avgparamdf2_high<-avgparamdf_high %>% dplyr::rename(ar1=q.mi.ar1, intercept=q.mi.intercept, light=q.mi.xreg1, discharge=q.mi.xreg2) %>%  separate(missingprop_autocor, into = c('missingprop1', 'autoCorr1'), sep = 16) %>% select(missingprop1, autoCorr1, ar1, intercept, light, discharge)  %>% mutate(missingness="MAR high auto") %>% mutate(type="Multiple imputations")
+
+avglSEdf2_high<-avglSEdf_high  %>% dplyr::rename(ar1=se.mi.ar1, intercept=se.mi.intercept, light=se.mi.xreg1, discharge=se.mi.xreg2) %>%  separate(missingprop_autocor, into = c('missingprop1', 'autoCorr1'), sep = 16) %>% select(missingprop1, autoCorr1, ar1, intercept, light, discharge)  %>% mutate(missingness="MAR high auto") %>% mutate(type="Multiple imputations")
+
+
+## pull out missing prop and autocorr values ##
+
+avgparamdf2_high$missingprop<-as.numeric(str_extract(avgparamdf2_high$missingprop1, "\\d+\\.*\\d*"))
+
+avgparamdf2_high$autocorr<-as.numeric(str_extract(avgparamdf2_high$autoCorr1, "\\d+\\.*\\d*"))
+
+avglSEdf2_high$missingprop<-as.numeric(str_extract(avglSEdf2_high$missingprop1, "\\d+\\.*\\d*"))
+
+avglSEdf2_high$autocorr<-as.numeric(str_extract(avglSEdf2_high$autoCorr1, "\\d+\\.*\\d*"))
+
+
+
+paramMIlong_high <- gather(avgparamdf2_high, param, value, ar1:discharge, factor_key=TRUE)
+
+paramMISElong_high <- gather(avglSEdf2_high, param, SE, ar1:discharge, factor_key=TRUE)
+
+paramMIlong_high2<-merge(paramMIlong_high,paramMISElong_high)
 
 
 ############ MISSING NOT AT RANDOM MNAR ##############################
@@ -62,26 +110,30 @@ paramlistsimMNAR<-arima_mi_MNAR[[1]]
 selistsimMNAR<-arima_mi_MNAR[[2]]
 
 
-avgparamdfMNAR <- map_df(paramlistsimMNAR, ~as.data.frame(.x), .id="missingprop")
-avglSEdfMNAR <- map_df(selistsimMNAR, ~as.data.frame(.x), .id="missingprop")
-
-missingprop<-seq(from=0.00, to =0.95, by=0.05)
-
-avgparamdfMNAR2<-avgparamdfMNAR %>% dplyr::rename(ar1=q.mi.ar1, intercept=q.mi.intercept, light=q.mi.xreg1, discharge=q.mi.xreg2) %>% select(ar1, intercept, light, discharge) %>% mutate(type="Multiple imputations")
-
-avglSEdfMNAR2<-avglSEdfMNAR  %>% dplyr::rename(ar1=se.mi.ar1, intercept=se.mi.intercept, light=se.mi.xreg1, discharge=se.mi.xreg2) %>% select(ar1, intercept, light, discharge) %>% mutate(type="Multiple imputations")
+avgparamdfMNAR <- map_df(paramlistsimMNAR, ~as.data.frame(.x), .id="missingprop_autocor")
+avglSEdfMNAR <- map_df(selistsimMNAR, ~as.data.frame(.x), .id="missingprop_autocor")
 
 
-missingprop<-seq(0.00, 0.95, by=0.05)
+avgparamdfMNAR2<-avgparamdfMNAR %>% dplyr::rename(ar1=q.mi.ar1, intercept=q.mi.intercept, light=q.mi.xreg1, discharge=q.mi.xreg2) %>%  separate(missingprop_autocor, into = c('missingprop1', 'autoCorr1'), sep = 16) %>% select(missingprop1, autoCorr1, ar1, intercept, light, discharge)  %>% mutate(missingness="MNAR") %>% mutate(type="Multiple imputations")
 
-MIdfMNAR2<-cbind(missingprop, avgparamdfMNAR2)
-
-MISEdfMNAR2<-cbind(missingprop, avglSEdfMNAR2)
+avglSEdfMNAR2<-avglSEdfMNAR  %>% dplyr::rename(ar1=se.mi.ar1, intercept=se.mi.intercept, light=se.mi.xreg1, discharge=se.mi.xreg2)%>%  separate(missingprop_autocor, into = c('missingprop1', 'autoCorr1'), sep = 16) %>% select(missingprop1, autoCorr1, ar1, intercept, light, discharge)  %>% mutate(missingness="MNAR")%>% mutate(type="Multiple imputations")
 
 
-paramMIlongMNAR <- gather(MIdfMNAR2, param, value, ar1:discharge, factor_key=TRUE)
+avgparamdfMNAR2$missingprop<-as.numeric(str_extract(avgparamdfMNAR2$missingprop1, "\\d+\\.*\\d*"))
 
-paramMISElongMNAR <- gather(MISEdfMNAR2, param, SE, ar1:discharge, factor_key=TRUE)
+avgparamdfMNAR2$autocorr<-as.numeric(str_extract(avgparamdfMNAR2$autoCorr1, "\\d+\\.*\\d*"))
+
+avglSEdfMNAR2$missingprop<-as.numeric(str_extract(avglSEdfMNAR2$missingprop1, "\\d+\\.*\\d*"))
+
+avglSEdfMNAR2$autocorr<-as.numeric(str_extract(avglSEdfMNAR2$autoCorr1, "\\d+\\.*\\d*"))
+
+
+
+# long form #
+
+paramMIlongMNAR <- gather(avgparamdfMNAR2, param, value, ar1:discharge, factor_key=TRUE)
+
+paramMISElongMNAR <- gather(avglSEdfMNAR2, param, SE, ar1:discharge, factor_key=TRUE)
 
 paramMIlongMNAR2<-merge(paramMIlongMNAR,paramMISElongMNAR)
 
