9b6a27919282740619dde09ebc1e1f8af917452e
Melissa <mdesierv@uwyo.edu>
2023-07-19 10:20:05 -0600

    updated GPP figure
    Former-commit-id: c0445448ee28f87b67e0f91082744557aa0a3d17

--- GPP_output_plots.R
+++ GPP_output_plots.R
@@ -23,11 +23,15 @@ source("Functions/Arima_drop_function.R")
 
 source("Model Runs/Data_Deletion_ARIMA.R")
 
+head(paramdropall)
+
 ## ARIMA method 2: Kalman filter (preserves the NAS) ###
 
 source("Functions/Arima_Kalman_function.R")
 
 source("Model Runs/Kalman_ARIMA.R")
+
+head(paramKalmanall)
 
 ## ARIMA method 3: Multiple imputations with AMELIA ###
 
@@ -36,23 +40,26 @@ source("Functions/Arima_MI_function.R")
 
 source("Model Runs/MI_ARIMA.R")
 
+head(paramMIall)
+
 ########## Run code for ARIMA methods over GPP 1 simulated dataset w/ increasing missingness #####
 
 source("Functions/model_fitting.R")
 
 source("Model Runs/DA_BRMS_GPP.R") ## COMPUTER CRASHES WHEN i RUN THIS ALL AT ONCE ##
 
+##ran them seperately and saved .csv. combining them all together back in here #
+
+BRMSall<-rbind(BRMSMARlowauto, BRMSMARhighauto, BRMSMNAR)
+
+head(BRMSall)
 
 
+##### FORMATTING ALL ###
 
-############# MISSING at RANDOM (MAR)#########################################
-
-
-#### Read in the missing dataframes that Alice S. made #####
-###### Pull out the first in 1000 nested lists for this code ### (Eventually loop over all the lists)
+### Estimates without missing data ####
 
 gauss_sim_MAR_datasets <- readRDS("data/Missingdatasets/gauss_sim_randMiss_autoCorr_10.rds")
-
 
 ## no missing###
 
@@ -79,15 +86,6 @@ simGPP <- ggplot(sim1df, aes(x=days, y=GPP))+
   labs(y=expression('GPP (g '*~O[2]~ m^-2~d^-1*')'), title="Simulated GPP")
 
 
-
-##For nested list of GPP datasets with increasing MAR data add back in the date column and the covariates## 
-
-GPP_sim_MAR<- gauss_sim_MAR_datasets [[1]][["y"]]
-
-GPP_sim_MAR_2 <-lapply(X = GPP_sim_MAR, FUN = function(X)   cbind.data.frame(GPP=X, days=sim1df$days, light = sim1df$light, discharge = sim1df$discharge))
-
-
-
 #########################################################################################################################
 ## ARIMA estimate with no missing data ##
 ###########################################################################################################################
@@ -102,12 +100,8 @@ Arimanomissing
 arimaestdf <- data.frame (param = c("ar1", "intercept", "light", "discharge"), value =Arimanomissing$coef)
 
 
-paramdroplong2 
-paramMIlong2
-paramNAlong2 
 
-paramallARIMA<-rbind(paramdroplong2, paramMIlong2, paramNAlong2)
-
+###################  PLOTTING #######################
 
 #### plotting ARIMA AND DATA AUGMENTATION (STAN) TOGETHER ###
 
@@ -116,9 +110,9 @@ paramallARIMA<-rbind(paramdroplong2, paramMIlong2, paramNAlong2)
 
 #combine all the arima outputs ##
 
-paramallARIMA<-rbind(paramdroplong2,paramNAlong2,paramMIlong2)
+paramallARIMA<-rbind(paramdropall,paramKalmanall,paramMIall)
 
-paramallARIMA<-paramallARIMA %>% select(type, param, value, SE, missingprop,autocorr)
+paramallARIMA<-paramallARIMA %>% select(type,missingness, param, value, SE, missingprop,autocorr)
 
 ## change AR 1 to be phi ##
 
@@ -134,89 +128,59 @@ paramallARIMA2$type <- str_replace(paramallARIMA2$type, "Kalman filter", "Kalman
 
 #### add ARIMA and STAN dataframes together ###
 
-paramallARIMASTAN<-rbind(paramallARIMA2, brmsparamdf2)
+paramallARIMASTAN<-rbind(paramallARIMA2, BRMSall[,2:8])
 
 ## drop sdp for comparison with arima ###
 paramallARIMASTAN2<- paramallARIMASTAN %>% filter(param != "sigma" ) 
 
-trueestdf2 <- data.frame (param = c("phi", "intercept", "light", "discharge"), value = c(realphi, realbetas))
 
 
 #paramallARIMASTAN2$type <- factor(paramallARIMASTAN2$type, levels=c("Data Deletion: Arima", "Kalman filter: Arima (default)", "Multiple imputations: Arima", "Data augmentation: STAN"), ordered=TRUE)
 
-arimastanMAR<-ggplot(data=paramallARIMASTAN2, aes(x=as.numeric(missingprop), y=value))+
-  facet_grid(~factor(param, levels=c("intercept", "phi", "light", "discharge"),exclude = NA)~ type, scales="free_y")+
-  geom_hline(data=trueestdf2, aes(yintercept=value), colour="gray")+
+#arimastanMAR<-ggplot(data=paramallARIMASTAN2, aes(x=as.numeric(missingprop), y=value))+
+  #facet_grid(~factor(param, levels=c("intercept", "phi", "light", "discharge"),exclude = NA)~ type, scales="free_y")+
+  #geom_hline(data=trueestdf2, aes(yintercept=value), colour="gray")+
   #geom_hline(data=arimaestdf, aes(yintercept=value), colour="light blue")+
-  geom_point(size=1)+
-  geom_errorbar(aes(ymin=value-SE, ymax=value+SE), size=0.3)+
-  theme_bw()+
-  xlab("Percent of Missing Data (Missing at Random)")+
-  ylab("Parameter estimate")+ theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.text.x = element_text(size = 6))
+  #geom_point(size=1)+
+  #geom_errorbar(aes(ymin=value-SE, ymax=value+SE), size=0.3)+
+  #theme_bw()+
+  #xlab("Percent of Missing Data (Missing at Random)")+
+  #ylab("Parameter estimate")+ theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.text.x = element_text(size = 6))
 
 
-pdf(file = "Figures/MARcomparison.pdf",width = 8, height = 5)
-arimastanMAR
-dev.off()
+#pdf(file = "Figures/MARcomparison.pdf",width = 8, height = 5)
+#arimastanMAR
+#dev.off()
+
+trueestdf2 <- data.frame (param = c("phi", "intercept", "light", "discharge"), value = c(realphi, realbetas))
+
+paramallARIMASTAN2$missingness <- factor(paramallARIMASTAN2$missingness, levels = c("MAR low auto", "MAR high auto", "MNAR"))
+
+# filter missingness > 0.77 ##
+
+paramallARIMASTAN3<-paramallARIMASTAN2 %>% filter(missingprop < 0.77)%>% filter(missingprop!= 0.22)%>% filter(missingprop!= 0.24)
+
 
 
 ### different version of the same figure ##
 
-arimastanMAR2<-ggplot(data=paramallARIMASTAN2, aes(x=as.numeric(missingprop), y=value, color=type))+
-  facet_wrap(~factor(param, levels=c("intercept", "phi", "light", "discharge"),exclude = NA),nrow=4, scales="free_y")+
+allmethodsGPP<-ggplot(data=paramallARIMASTAN3, aes(x=as.numeric(missingprop), y=value, color=type))+
+  facet_grid(~factor(param, levels=c("intercept", "phi", "light", "discharge"),exclude = NA)~ missingness, scales="free_y")+
   geom_hline(data=trueestdf2, aes(yintercept=value), colour="gray")+
   #geom_hline(data=arimaestdf, aes(yintercept=value), colour="light blue")+
-  geom_point(size=1, position = position_dodge(width=0.02))+
-  geom_errorbar(aes(ymin=value-SE, ymax=value+SE), size=0.3, position = position_dodge(width=0.02))+
+  geom_point(size=0.75, position = position_dodge(width=0.03))+
+  geom_errorbar(aes(ymin=value-SE, ymax=value+SE), size=0.3, width=0, position = position_dodge(width=0.03))+
   theme_bw()+
-  xlab("Percent of Missing Data (Missing at Random)")+
-  ylab("Parameter estimate")+ theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.text.x = element_text(size = 6))+ggtitle("MAR low autocorrelation (~0.10)")
+  xlab("Proportion of missing data")+ theme(legend.position="top")+theme(legend.title=element_blank())+
+  ylab("Parameter estimate")+ theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.text.x = element_text(size = 8))
 
 
-pdf(file = "Figures/MARcomparison2.pdf",width = 8, height = 5)
-arimastanMAR2
+pdf(file = "Figures/MARautoMNAR.pdf",width = 8, height = 5)
+allmethodsGPP
 dev.off()
 
 
 
 
-################################ MISSING NOT AT RANDOM (MNAR) ##########################
-
-## no missing###
-
-sim1MNAR<-gauss_sim_MNAR_datasets [[1]][["y"]][["y_noMiss"]]
-
-realphiMNAR<-gauss_sim_MNAR_datasets[[1]][["sim_params"]][["phi"]]
-realbetasMNAR<-gauss_sim_MNAR_datasets[[1]][["sim_params"]][["beta"]]
-
-trueestdfMNAR <- data.frame (param = c("ar1", "intercept", "light", "discharge"), value = c(realphiMNAR, realbetasMNAR))
-
-covariates<-gauss_sim_MNAR_datasets[[1]][["sim_params"]][["X"]]
-
-covariatesX<-as.matrix(covariates[,2:3])
-
-days<-seq(1, 365)
-
-sim1MNARdf<-as.data.frame(cbind(days=days, GPP=sim1MNAR, light=covariates[,2], discharge=covariates[,3]))
-
-#########################################################################################################################
-## ARIMA estimate with no missing data ##
-###########################################################################################################################
-
-##matrix of covariates##
-X = matrix(c(sim1df$light,  sim1df$discharge), ncol = 2)
-
-Arimanomissing=arima(sim1df$GPP, order = c(1,0,0), xreg = X)
-
-Arimanomissing
-
-arimaestdf <- data.frame (param = c("ar1", "intercept", "light", "discharge"), value =Arimanomissing$coef)
-
-
-#### plotting ARIMA AND DATA AUGMENTATION (STAN) TOGETHER ###
-
-#### DATA AUGMENTATION WITH BRMS ##########
-
-
-gauss_sim_MNAR_datasets <- readRDS(here("data/Missingdatasets/gauss_sim_minMaxMiss.rds"))
+####
 
