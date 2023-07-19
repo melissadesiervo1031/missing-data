bb15034d36ed341e12570e71a74484ff70e72285
aestears <astears@uwyo.edu>
2023-07-19 14:52:55 -0600

    made first pass of Guassian missing data types figure
    Former-commit-id: 59d7816c554c1cdb545be7ecacc9c04b96bcd47a

--- Simulations/MissingnessTypes_figure.R
+++ Simulations/MissingnessTypes_figure.R
@@ -38,125 +38,52 @@ gausSim$y_minMaxMiss_40 <- as.vector(unlist(makeMissing(timeSeries = gausSim$y, 
 # get 10% missing in minmax of data
 gausSim$y_minMaxMiss_10 <- as.vector(unlist(makeMissing(timeSeries = gausSim$y, typeMissing = "minMax", propMiss = .1)))
 
+gausSim$density_x <- seq(from = -5, to = 7, length.out = nrow(gausSim))
+gausSim$density_y <- dnorm(gausSim$density_x, mean = mean(gausSim$y), sd = sd(gausSim$y))
+
+## put data into a long data.frame
+gausSim <- gausSim %>% 
+  pivot_longer(cols = c(y, y_lowAuto_40Miss, y_lowAuto_10Miss, y_highAuto_10miss, y_highAuto_40miss, y_minMaxMiss_40, y_minMaxMiss_10),
+               names_to = "MissingnessType")
+# fix names
+gausSim[gausSim$MissingnessType == "y", "MissingnessType"] <- "A: No Missing Data"
+gausSim[gausSim$MissingnessType == "y_lowAuto_40Miss", "MissingnessType"] <- "D: MAR: 40% missing, .01 autocorr."
+gausSim[gausSim$MissingnessType == "y_lowAuto_10Miss", "MissingnessType"] <- "B: MAR: 10% missing, .01 autocorr."
+gausSim[gausSim$MissingnessType == "y_highAuto_10miss", "MissingnessType"] <- "C: MAR: 10% missing, .9 autocorr."
+gausSim[gausSim$MissingnessType == "y_highAuto_40miss", "MissingnessType"] <- "E: MAR: 40% missing, .9 autocorr."
+gausSim[gausSim$MissingnessType == "y_minMaxMiss_10", "MissingnessType"] <- "F: MNAR: 10% missing"
+gausSim[gausSim$MissingnessType == "y_minMaxMiss_40", "MissingnessType"] <- "G: MNAR: 40% missing"
+
+gausSim$MissingnessType <- factor(gausSim$MissingnessType, 
+                                      levels = c("A: No Missing Data", "B: MAR: 10% missing, .01 autocorr.", "C: MAR: 10% missing, .9 autocorr.",
+                                                 "D: MAR: 40% missing, .01 autocorr.", "E: MAR: 40% missing, .9 autocorr.", "F: MNAR: 10% missing", "G: MNAR: 40% missing"),
+                                      ordered = TRUE)
 # make histograms like Dusty made (looking at histogram of values in the missing
 # data time series vs. expected distribution from the AR1 process)
 # no missing data
-gauss_noMiss_line <- ggplot(data = gausSim, aes(x = time, y = y)) +
+
+ts_lines <- ggplot(data = gausSim, aes(x = time, y = value)) +
   geom_line() +
   geom_point(size = 1) +
-  ggtitle("No Missing Data") +
-  ylab("y") + 
-  theme_classic()
-
-gauss_noMiss_hist <- ggplot() +
-  geom_histogram(data = gausSim, aes(y, after_stat(density)), fill = "grey", color = "darkgrey") +
-  geom_line(aes(y = dnorm(seq(-5,7,.1), mean = mean(gausSim$y), sd = sd(gausSim$y)),
-                x = seq(-5,7,.1)), color = "blue") +
-  xlab("y") +
-  theme_classic()
-
-# randomly missing (low autocorrellation, 10% missing)
-gauss_lowAuto_10Miss_line <- ggplot(data = gausSim, aes(x = time, y = y_lowAuto_10Miss)) +
-  geom_line() +
-  geom_point(size = 1) +
-  ggtitle("Missing at Random: 10% missing, autoCorr = .01") +
+  facet_wrap(~MissingnessType, ncol = 1 ) + 
   ylab("y") +
-  theme_classic()
+  theme_classic() +
+  theme(strip.text = element_text(hjust = 0))
 
-gauss_lowAuto_10Miss_hist <- ggplot() +
-  geom_histogram(data = gausSim, aes(y_lowAuto_10Miss, after_stat(density)), fill = "grey", color = "darkgrey") +
-  geom_line(aes(y = dnorm(seq(-5,7,.1), mean = mean(gausSim$y), sd = sd(gausSim$y)),
-                x = seq(-5,7,.1)), color = "blue") +
-  xlab("y") +
-  theme_classic()
+ts_hist <- ggplot(data = gausSim) +
+  geom_histogram(aes(value, after_stat(density)), fill = "grey", color = "darkgrey") +
+  geom_line(aes(y = density_y, x = density_x), color = "blue") +
+  facet_wrap(~MissingnessType, ncol = 1) + 
+  theme_classic() +
+  xlab("y") + 
+  theme(strip.text = element_blank())
 
-# randomly missing (low autocorrellation, 40% missing)
-gauss_lowAuto_40Miss_line <- ggplot(data = gausSim, aes(x = time, y = y_lowAuto_40Miss)) +
-  geom_line() +
-  geom_point(size = 1) +
-  ggtitle("Missing at Random: 40% missing, autoCorr = .01") +
-  ylab("y") +
-  theme_classic()
-
-gauss_lowAuto_40Miss_hist <- ggplot() +
-  geom_histogram(data = gausSim, aes(y_lowAuto_40Miss, after_stat(density)), fill = "grey", color = "darkgrey") +
-  geom_line(aes(y = dnorm(seq(-5,7,.1), mean = mean(gausSim$y), sd = sd(gausSim$y)),
-                x = seq(-5,7,.1)), color = "blue") +
-  xlab("y") +
-  theme_classic()
-
-# missing randomly spaced chunks (high autocorr, 10% missing)
-gauss_highAuto_10Miss_line <- ggplot(data = gausSim, aes(x = time, y = y_highAuto_10miss)) +
-  geom_line() +
-  geom_point(size = 1) +
-  ggtitle("Missing at Random: 10% missing, autoCorr = .9") +
-  ylab("y") +
-  theme_classic()
-
-gauss_highAuto_10Miss_hist <- ggplot() +
-  geom_histogram(data = gausSim, aes(y_lowAuto_10Miss, after_stat(density)), fill = "grey", color = "darkgrey") +
-  geom_line(aes(y = dnorm(seq(-5,7,.1), mean = mean(gausSim$y), sd = sd(gausSim$y)),
-                x = seq(-5,7,.1)), color = "blue") +
-  xlab("y") +
-  theme_classic()
-
-# missing randomly spaced chunks (high autocorr, 40% missing)
-gauss_highAuto_40Miss_line <- ggplot(data = gausSim, aes(x = time, y = y_highAuto_40miss)) +
-  geom_line() +
-  geom_point(size = 1) +
-  ggtitle("Missing at Random: 40% missing, autoCorr = .9") +
-  ylab("y") +
-  theme_classic()
-
-gauss_highAuto_40Miss_hist <- ggplot() +
-  geom_histogram(data = gausSim, aes(y_lowAuto_40Miss, after_stat(density)), fill = "grey", color = "darkgrey") +
-  geom_line(aes(y = dnorm(seq(-5,7,.1), mean = mean(gausSim$y), sd = sd(gausSim$y)),
-                x = seq(-5,7,.1)), color = "blue") +
-  xlab("y") +
-  theme_classic()
-
-# missing at min and max (10%)
-gauss_minMaxMiss_10miss_line <- ggplot(data = gausSim, aes(x = time, y = y_minMaxMiss_10)) +
-  geom_line() +
-  geom_point(size = 1) +
-  ggtitle("Missing Not at Random: 10% missing") +
-  ylab("y") +
-  ylim(c(-2.5,5)) +
-  theme_classic()
-
-gauss_minMaxMiss_10miss_hist <- ggplot() +
-  geom_histogram(data = gausSim, aes(y_minMaxMiss_10, after_stat(density)), fill = "grey", color = "darkgrey") +
-  geom_line(aes(y = dnorm(seq(-5,7,.1), mean = mean(gausSim$y), sd = sd(gausSim$y)),
-                x = seq(-5,7,.1)), color = "blue") +
-  xlab("y") +
-  theme_classic()
-
-# missing at min and max (40%)
-gauss_minMaxMiss_40miss_line <- ggplot(data = gausSim, aes(x = time, y = y_minMaxMiss_40)) +
-  geom_line() +
-  geom_point(size = 1) +
-  ggtitle("Missing Not at Random: 40% missing") +
-  ylab("y") +
-  ylim(c(-2.5,5)) +
-  theme_classic()
-
-gauss_minMaxMiss_40miss_hist <- ggplot() +
-  geom_histogram(data = gausSim, aes(y_minMaxMiss_40, after_stat(density)), fill = "grey", color = "darkgrey") +
-  geom_line(aes(y = dnorm(seq(-5,7,.1), mean = mean(gausSim$y), sd = sd(gausSim$y)),
-                x = seq(-5,7,.1)), color = "blue") +
-  xlab("y") +
-  theme_classic()
 
 ## save the figure to file
 png(filename = "./figures/CompareMissingnessTypes_fig.png", width = 700, height = 600)
-ggarrange(gauss_noMiss_line, gauss_noMiss_hist,
-          gauss_lowAuto_10Miss_line, gauss_lowAuto_10Miss_hist,
-          gauss_lowAuto_40Miss_line, gauss_lowAuto_40Miss_hist,
-          gauss_highAuto_10Miss_line, gauss_highAuto_10Miss_hist,
-          gauss_highAuto_40Miss_line, gauss_highAuto_40Miss_hist,
-          gauss_minMaxMiss_10miss_line, gauss_minMaxMiss_10miss_hist,
-          gauss_minMaxMiss_40miss_line, gauss_minMaxMiss_40miss_hist,
-          widths = c(.75,.25, .75,.25, .75,.25, .75,.25, .75,.25,.75,.25,.75,.25),
+ggarrange(ts_lines,
+          ts_hist,
+          widths = c(.75,.25),
           ncol = 2,
-          nrow = 7)
+          nrow = 1)
 dev.off()
