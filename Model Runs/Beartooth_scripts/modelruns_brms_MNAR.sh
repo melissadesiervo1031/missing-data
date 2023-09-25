df9512511bae3370e2af4868a92df7471ae820ce
aestears <astears@uwyo.edu>
2023-09-25 15:27:40 -0600

    put all gaussian simulated data model results together into one csv
    Former-commit-id: 7411ba4711d10eda9375a0ccd164f3ff96fa0eb1
    Former-commit-id: 018045d577e27452dbbbe0f9c5c1ca339eea00e5

---
+++ Model Runs/Beartooth_scripts/modelruns_brms_MNAR.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+#SBATCH --job-name modelruns_brms_MNAR
+#SBATCH --time=12:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=astears@uwyo.edu
+#SBATCH --account=modelscape
+#SBATCH --output=output/modelruns_brms_MNAR_ex01_%A.out
+#SBATCH --array=0-1
+#SBATCH --mem=3G           # total memory per node
+
+echo "SLURM_JOB_ID:" $SLURM_JOB_ID
+echo "SLURM_JOB_NAME:" $SLURM_JOB_NAME
+echo "SLURM_JOB_NODELIST:" $SLURM_JOB_NODELIST
+echo "SLURM_ARRAY_TASK_ID:" $SLURM_ARRAY_TASK_ID
+
+module load gcc/12.2.0 arcc/1.0 r/4.2.2
+
+cd /project/modelscape/users/astears
+
+Rscript --vanilla modelruns_brms_MNAR.R $SLURM_ARRAY_TASK_ID
+
