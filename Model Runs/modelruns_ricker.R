441d8581b06f434cc337b5692685d8cab7690e62
Amy Patterson <amypatterson@Amys-MBP.lan>
2023-11-19 01:38:22 -0700

    adding array ability for ricker simulations

--- Model Runs/modelruns_ricker.R
+++ Model Runs/modelruns_ricker.R
@@ -22,6 +22,7 @@ lapply(f_list, source)
 
 # arguments from the shell
 # Should include (1) data file (2) parameter file (3) number of nodes for cluster (4) save file location
+# (5) beginnning index (6) ending index (7) optional- model list
 in_args <- commandArgs(trailingOnly = T)
 
 # read in datafile
@@ -115,14 +116,14 @@ if(!is.null(names(dat))){
 # pars_complete <- pars_full[-probs, ]
 
 # time testing only
-dat_flat <- dat_flat[1:1000]
-pars_full <- pars_full[1:1000, ]
+dat_flat <- dat_flat[as.numeric(in_args[5]):as.numeric(in_args[6])]
+pars_full <- pars_full[as.numeric(in_args[5]):as.numeric(in_args[6]),]
 
 
 ### fitting the models ###
 
 # define methods to be used
-if(is.na(in_args[5])){
+if(is.na(in_args[7])){
   methods <- paste0(
     c("drop", "cc", "EM", "DA","MI")
   )
--- Model Runs/run_modelruns_ricker.sh
+++ Model Runs/run_modelruns_ricker.sh
@@ -2,19 +2,30 @@
 
 #SBATCH --account=modelscape
 #SBATCH --nodes=1
-#SBATCH --time=48:00:00
+#SBATCH --time=24:00:00
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=3
+#SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=8G
 #SBATCH --mail-type=ALL
 ### please enter your own email address below in order to track the results
 #SBATCH --mail-user=apatte12@uwyo.edu
 ### enter any job name that you prefer
 #SBATCH --job-name=rickerTestRun
+#SBATCH --array=1-30
 
 
 module load arcc/1.0 gcc/12.2.0 r/4.2.2
 
 cd /project/modelscape/analyses/MissingTS/missing-data
 
-Rscript "Model Runs/modelruns_ricker.R" "data/missingDatasets/pois_sim_randMiss_A.rds" "data/missingDatasets/pois_sim_params.rds" 3 "Model Runs/RickerA_resultTable.rds" > "Model Runs/outputRickerA.txt"
+config="Model Runs/RickerConfig.txt"
+
+datFile=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $2}' $config)
+parFile=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $3}' $config)
+clsize=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $4}' $config)
+saveFile=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $5}' $config)
+index1=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $6}' $config)
+index2=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $7}' $config)
+
+
+Rscript "Model Runs/modelruns_ricker.R" ${datFile} ${parFile} ${clsize} ${saveFile} ${index1} ${index2} > "Model Runs/outputRickerA_${SLURM_ARRAY_TASK_ID}.txt" 
