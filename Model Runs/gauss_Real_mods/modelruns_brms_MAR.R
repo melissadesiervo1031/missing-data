37cc17247207529b22cd5081219ed7498afccabf
aestears <astears@uwyo.edu>
2023-11-28 15:55:58 -0700

    changing prior for phi in brms models to normal(0,1) (from uniform(0,1))
    Former-commit-id: 84db6943eb29ab0a974905b64775addefccad3f2

--- Model Runs/gauss_Real_mods/modelruns_brms_MAR.R
+++ Model Runs/gauss_Real_mods/modelruns_brms_MAR.R
@@ -47,7 +47,7 @@ fit_brms_model <- function(sim_list, sim_pars,
   
   # Make the model formula and priors
   bform <- brms::bf(GPP | mi() ~ light + Q + ar(p = 1))
-  bprior <- c(prior(uniform(0,1), class = 'ar', lb = 0, ub = 1),
+  bprior <- c(prior(normal(0,1), class = 'ar', lb = 0, ub = 1),
               prior(normal(0,5), class = 'b'))
   
   # fit model to list of datasets
