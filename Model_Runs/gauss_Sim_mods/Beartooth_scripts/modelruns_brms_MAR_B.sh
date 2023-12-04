bd3a531ec7cf3b5fc278682652fdc871c763f69f
aestears <astears@uwyo.edu>
2023-12-04 09:43:05 -0700

    update gaussian real sim brms code to run models w/ normal prior (rather than uniform)

---
+++ Model_Runs/gauss_Sim_mods/Beartooth_scripts/modelruns_brms_MAR_B.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+#SBATCH --job-name modelruns_brms
+#SBATCH --time=12:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=astears@uwyo.edu
+#SBATCH --account=modelscape
+#SBATCH --output=modelruns_brms_MAR_ex01_%A.out
+#SBATCH --array=0-4999
+
+echo "SLURM_JOB_ID:" $SLURM_JOB_ID
+echo "SLURM_JOB_NAME:" $SLURM_JOB_NAME
+echo "SLURM_JOB_NODELIST:" $SLURM_JOB_NODELIST
+echo "SLURM_ARRAY_TASK_ID:" $SLURM_ARRAY_TASK_ID
+
+module load gcc/12.2.0 arcc/1.0 r/4.2.2
+
+cd /project/modelscape/users/acarte26/missing-data
+
+Rscript --vanilla modelruns_brms_MAR.R $SLURM_ARRAY_TASK_ID
+
