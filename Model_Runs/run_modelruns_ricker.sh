02d12bd46862051fbecd048fa858333abfba87fd
Amy Patterson <amypatterson@Amys-MBP.lan>
2023-12-14 23:44:58 -0700

    ricker B reruns
    Former-commit-id: e42b105ac24fda8e29d183ca45d3630a1ff3e849

--- Model_Runs/run_modelruns_ricker.sh
+++ Model_Runs/run_modelruns_ricker.sh
@@ -5,13 +5,13 @@
 #SBATCH --time=24:00:00
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=5
-#SBATCH --mem-per-cpu=8G
+#SBATCH --mem-per-cpu=4G
 #SBATCH --mail-type=ALL
 ### please enter your own email address below in order to track the results
 #SBATCH --mail-user=apatte12@uwyo.edu
 ### enter any job name that you prefer
 #SBATCH --job-name=rickerTestRun
-#SBATCH --array=1-6
+#SBATCH --array=1-1000%100
 
 
 module load arcc/1.0 gcc/12.2.0 r/4.2.2
