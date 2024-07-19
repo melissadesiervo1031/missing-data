78a10f09017f196333bc6e5e23003f6c87c9f356
Dusty Gannon <dustygannon@MACKLEMORE.forestry.oregonstate.edu>
2024-07-19 15:52:23 -0700

    Think I finally fixed the ricker_DA function
    Former-commit-id: 7c1fafe1fb92e5dd2328372147bfeb255bf62835

--- ModelChecking/run_bias_checks_expanded.sh
+++ ModelChecking/run_bias_checks_expanded.sh
@@ -3,20 +3,21 @@
 #SBATCH --account=modelscape
 #SBATCH --time=5:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks-per-node=26
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=32G
+#SBATCH --mem=24G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=dgannon@uwyo.edu
 
 # Set the parameter combination to use and generate names of R scripts and log file
-Rscript=fit_lnorm_ricker_vrtests_tandem.R
-LogFile=ricker_vrtests
+Rscript=Bias_checks_expanded.R
+LogFile=bias_checks.log
 
 # Change to the relevant working directory
-cd /project/modelscape/analyses/sponges/Analyses
+cd /project/modelscape/analyses/MissingTS/missing-data/ModelChecking
 
 # Load R
-module load arcc/1.0  gcc/12.2.0 r/4.2.2
+module load arcc/1.0  gcc/12.2.0 r/4.4.0
 
-Rscript --vanilla $Rscript lnorm_ricker_sims_200steps_rho0_S5_s55.rds 51 100 vr_tests.rds > $LogFile
+Rscript --vanilla $Rscript > $LogFile
+
