7fa794d1a6fda96ada3d6896424f0b860e36b53d
apatt76 <amy.patterson76@gmail.com>
2024-10-24 00:26:09 -0600

    shell script updated with slightly higher time to accomodate increased burnin

--- Model_Runs/poiss_Sim_mods/run_modelruns_rickerA.sh
+++ Model_Runs/poiss_Sim_mods/run_modelruns_rickerA.sh
@@ -2,10 +2,10 @@
 
 #SBATCH --account=modelscape
 #SBATCH --nodes=1
-#SBATCH --time=48:00:00
+#SBATCH --time=72:00:00
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=5
-#SBATCH --mem-per-cpu=4G
+#SBATCH --mem-per-cpu=8G
 #SBATCH --mail-type=ALL
 ### please enter your own email address below in order to track the results
 #SBATCH --mail-user=apatte12@uwyo.edu
