7e29b6584c5de6688dca5be9acfc76554095b0a5
apatt76 <amy.patterson76@gmail.com>
2026-05-11 17:20:15 -0400

    K est and error and forecast 2 MNAR

--- Model_Runs/poiss_Sim_mods/K_est_forecast2.R
+++ Model_Runs/poiss_Sim_mods/K_est_forecast2.R
@@ -20,6 +20,10 @@ forecast_rmse=function(ralpha,trueTS){
   return(RMSE)
 }
 
+#############################################################################
+#############################################################################
+# MCAR A and B
+
 ricDat_tempA <- readRDS("./data/model_results/RickerA_resultTableRev1.rds")
 ricDat_tempB <- readRDS("./data/model_results/RickerB_resultTableRev1.rds")
 
@@ -207,11 +211,14 @@ saveRDS(ricDat_temp[1:75000,],"./data/model_results/RickerA_resultTableRev1.rds"
 saveRDS(ricDat_temp[75001:15000,],"./data/model_results/RickerB_resultTableRev1.rds")
 saveRDS(ricker_forecast_sims,"./data/ricker_forecast_sim.rds")
 
+#############################################################################
+#############################################################################
+# MCAR MI A and B
+
 ricker_forecast_sims=readRDS("./data/ricker_forecast_sim.rds")
 ricDat_MIA<-readRDS("./data/model_results/RickerA_resultTableMIRev1.rds")
 ricDat_MIB<-readRDS("./data/model_results/RickerB_resultTableMIRev1.rds")
 ricDat_temp=rbind(ricDat_MIA,ricDat_MIB)
-
 
 ricDat_temp$K_true=ricDat_temp$r/ricDat_temp$alpha
 ricDat_temp$K_MI=NA
@@ -262,4 +269,199 @@ for(i in 1:nrow(ricDat_temp)){
 
 saveRDS(ricDat_temp[1:75000,],"./data/model_results/RickerA_resultTableMIRev1.rds")
 saveRDS(ricDat_temp[75001:15000,],"./data/model_results/RickerB_resultTableMIRev1.rds")
+
+
+#############################################################################
+#############################################################################
+# MNAR 
+ricDat_temp=readRDS("./data/model_results/RickerMinMaxMissRev1.rds")
+ricker_forecast_sims=readRDS("./data/ricker_forecast_sim.rds")
+
+ricDat_temp$K_true=ricDat_temp$r/ricDat_temp$alpha
+ricDat_temp$K_drop=NA
+ricDat_temp$K_cc=NA
+ricDat_temp$K_EM=NA
+ricDat_temp$K_DA=NA
+ricDat_temp$K_drop_err=NA
+ricDat_temp$K_cc_err=NA
+ricDat_temp$K_EM_err=NA
+ricDat_temp$K_DA_err=NA
+
+for(i in 1:nrow(ricDat_temp)){ # calculate and record estimated r/alpha for each method
+  print(paste("we are at i=",i))
+  K_drop_est=tryCatch({
+    ricDat_temp$drop_fits[[i]]$estim[1]/ricDat_temp$drop_fits[[i]]$estim[2]
+  },
+  error=function(e){
+    message("an error occurred",e$message)
+    return(NA)},
+  warning=function(w){
+    message("a warning occurred",w$message)
+  }
+  )
+  if(length(K_drop_est)==0){K_drop_est=NA}
+  ricDat_temp$K_drop[i]=K_drop_est
+  ricDat_temp$K_drop_err[i]=K_drop_est-ricDat_temp$K_true[i]
+  K_cc_est=tryCatch({
+    ricDat_temp$cc_fits[[i]]$estim[1]/ricDat_temp$cc_fits[[i]]$estim[2]
+  },
+  error=function(e){
+    message("an error occurred",e$message)
+    return(NA)},
+  warning=function(w){
+    message("a warning occurred",w$message)
+  }
+  )
+  
+  if(length(K_cc_est)==0){K_cc_est=NA}
+  ricDat_temp$K_cc[i]=K_cc_est
+  ricDat_temp$K_cc_err[i]=K_cc_est-ricDat_temp$K_true[i]
+  K_EM_est=tryCatch({
+    ricDat_temp$EM_fits[[i]]$estim[1]/ricDat_temp$EM_fits[[i]]$estim[2]
+  },
+  error=function(e){
+    message("an error occurred",e$message)
+    return(NA)},
+  warning=function(w){
+    message("a warning occurred",w$message)
+  }
+  )
+  
+  if(length(K_EM_est)==0){K_EM_est=NA}
+  ricDat_temp$K_EM[i]=K_EM_est
+  ricDat_temp$K_EM_err[i]=K_EM_est-ricDat_temp$K_true[i]
+  K_DA_est=tryCatch({
+    ricDat_temp$DA_fits[[i]]$estim[1]/ricDat_temp$DA_fits[[i]]$estim[2]
+  },
+  error=function(e){
+    message("an error occurred",e$message)
+    return(NA)},
+  warning=function(w){
+    message("a warning occurred",w$message)
+  }
+  )
+  if(length(K_DA_est)==0){K_DA_est=NA}
+  ricDat_temp$K_DA[i]=K_DA_est
+  ricDat_temp$K_DA_err[i]=K_DA_est-ricDat_temp$K_true[i]
+  
+}
+
+
+ricDat_temp$forecast_2_drop=NA
+ricDat_temp$forecast_2_cc=NA
+ricDat_temp$forecast_2_EM=NA
+ricDat_temp$forecast_2_DA=NA
+
+for(i in 1:nrow(ricDat_temp)){
+  print(paste("working on", i))
+  simnum=ricDat_temp$SimNumber[i]
+  trueTS=ricker_forecast_sims[[simnum]]
+  
+  # drop
+  ricDat_temp$forecast_2_drop[i]=tryCatch({
+    forecast_rmse(c(ricDat_temp$drop_fits[[i]]$estim[["r"]],ricDat_temp$drop_fits[[i]]$estim[["alpha"]]),trueTS)
+  },
+  error=function(e){
+    message("an error occurred",e$message)
+    return(NA)},
+  warning=function(w){
+    message("a warning occurred",w$message)
+  }
+  )
+  
+  # cc
+  ricDat_temp$forecast_2_cc[i]=tryCatch({
+    forecast_rmse(c(ricDat_temp$cc_fits[[i]]$estim[["r"]],ricDat_temp$cc_fits[[i]]$estim[["alpha"]]),trueTS)
+  },
+  error=function(e){
+    message("an error occurred",e$message)
+    return(NA)},
+  warning=function(w){
+    message("a warning occurred",w$message)
+  }
+  )
+  
+  # EM
+  ricDat_temp$forecast_2_EM[i]=tryCatch({
+    forecast_rmse(c(ricDat_temp$EM_fits[[i]]$estim[["r"]],ricDat_temp$EM_fits[[i]]$estim[["alpha"]]),trueTS)
+  },
+  error=function(e){
+    message("an error occurred",e$message)
+    return(NA)},
+  warning=function(w){
+    message("a warning occurred",w$message)
+  }
+  )
+  
+  
+  # DA
+  ricDat_temp$forecast_2_DA[i]=tryCatch({
+    forecast_rmse(c(ricDat_temp$DA_fits[[i]]$estim[["r"]],ricDat_temp$DA_fits[[i]]$estim[["alpha"]]),trueTS)
+  },
+  error=function(e){
+    message("an error occurred",e$message)
+    return(NA)},
+  warning=function(w){
+    message("a warning occurred",w$message)
+  }
+  )
+}
+
+saveRDS(ricDat_temp,"./data/model_results/RickerMinMaxMissRev1.rds")
+
+#############################################################################
+#############################################################################
+# MNAR MI
+ricDat_temp=readRDS("./data/model_results/RickerMinMaxMiss_MIRev1.rds")
+ricker_forecast_sims=readRDS("./data/ricker_forecast_sim.rds")
+
+
+ricDat_temp$K_true=ricDat_temp$r/ricDat_temp$alpha
+ricDat_temp$K_MI=NA
+ricDat_temp$K_MI_err=NA
+
+for(i in 1:nrow(ricDat_temp)){ # calculate and record estimated r/alpha for each method
+  print(paste("we are at i=",i))
+  K_MI_est=tryCatch({
+    as.numeric(ricDat_temp$estim_r[i])/as.numeric(ricDat_temp$estim_alpha[i])
+  },
+  error=function(e){
+    message("an error occurred",e$message)
+    return(NA)},
+  warning=function(w){
+    message("a warning occurred",w$message)
+  }
+  )
+  if(length(K_MI_est)==0){K_MI_est=NA}
+  ricDat_temp$K_MI[i]=K_MI_est
+  ricDat_temp$K_MI_err[i]=K_MI_est-ricDat_temp$K_true[i]
+  
+  
+}
+
+
+ricDat_temp$forecast_2_MI=NA
+
+for(i in 1:nrow(ricDat_temp)){
+  print(paste("working on", i))
+  simnum=ricDat_temp$SimNumber[i]
+  trueTS=ricker_forecast_sims[[simnum]]
+  
+  ricDat_temp$forecast_2_MI[i]=tryCatch({
+    forecast_rmse(c(as.numeric(ricDat_temp$estim_r[i]),as.numeric(ricDat_temp$estim_alpha[i])),trueTS)
+  },
+  error=function(e){
+    message("an error occurred",e$message)
+    return(NA)},
+  warning=function(w){
+    message("a warning occurred",w$message)
+    return(NA)
+  }
+  )
+  
+  
+}
+
+
+saveRDS(ricDat_temp,"./data/model_results/RickerMinMaxMiss_MIRev1.rds")
 
